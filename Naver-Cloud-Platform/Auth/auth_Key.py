7d04159eb93b6f6113f1dcb736033ebcd9704698
kong-sj <80231431+kong-sj@users.noreply.github.com>
2024-11-12 14:23:13 +0900

    Update auth_Key.py

--- Naver-Cloud-Platform/Auth/auth_Key.py
+++ Naver-Cloud-Platform/Auth/auth_Key.py
@@ -3,5 +3,5 @@
 # ncloud_secretkey = "ncp_iam_BFKSKRDJCq8wWVywTdxPlBNrsvAingMfBd"
 
 ## from koscom
-ncloud_accesskey = "D475C0F1F549B7B991D3"
-ncloud_secretkey = "F4F1C65429A18377D80853EA834C9EDA5652BEB1"
+ncloud_accesskey = ""
+ncloud_secretkey = ""
