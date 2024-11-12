58bd90ddc58e87191558cd035c936ceecf07cd2d
kong-sj <sungjun7305@gmail.com>
2024-11-12 15:24:55 +0900

    modify send_mail.py

--- Naver-Cloud-Platform/Storage/send_mail.py
+++ Naver-Cloud-Platform/Storage/send_mail.py
@@ -1,24 +1,8 @@
 def main(args):
-    import hashlib
-    import hmac
-    import base64
     import time
-    import requests
     from Auth.auth_Key import ncloud_accesskey, ncloud_secretkey
-
-    def	make_signature(access_key, secret_key, method, uri, timestamp):
-        timestamp = str(timestamp)
-        secret_key = bytes(secret_key, 'UTF-8')
-        message = method + " " + uri + "\n" + timestamp + "\n" + access_key
-        message = bytes(message, 'UTF-8')
-        signingKey = base64.b64encode(hmac.new(secret_key, message, digestmod=hashlib.sha256).digest()).decode()
-        return signingKey
-
-    def send_request(url, headers, method, data):
-        r = requests.request(method, url, headers=headers, json=data)
-        if r.status_code != 200:
-            print('ERROR! {} {}'.format(r.status_code, r.text))
-        return r.text
+    from Auth.signature import make_signature
+    from send_request import send_request
 
     def send_mail(bucket_name, object_name, t, event_type):
         timestamp = int(time.time()*1000)
