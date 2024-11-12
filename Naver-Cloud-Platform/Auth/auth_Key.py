3ae0d309e427fc257991af9ab4a0a03f4ea263c0
kong-sj <80231431+kong-sj@users.noreply.github.com>
2024-11-12 14:40:17 +0900

    Delete Naver-Cloud-Platform/Auth directory

--- Naver-Cloud-Platform/Auth/auth_Key.py
+++
@@ -1,7 +0,0 @@
-## from sjhong
-# ncloud_accesskey = "ncp_iam_BFASKR2l12lIt7545zGl"
-# ncloud_secretkey = "ncp_iam_BFKSKRDJCq8wWVywTdxPlBNrsvAingMfBd"
-
-## from koscom
-ncloud_accesskey = ""
-ncloud_secretkey = ""
